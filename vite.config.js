cfebbd01cff0aa6cdf56d09f94f17510a8f4d647
Kunal Kashyap <kunalkashyap8024@gmail.com>
2025-04-01 16:45:07 +0530

    hi

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -18,7 +18,18 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    // Generate source maps for better debugging
     sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html')
+      },
+      external: ['react-dom/client'],
+    }
+  },
+  resolve: {
+    alias: {
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+    }
   }
 });
